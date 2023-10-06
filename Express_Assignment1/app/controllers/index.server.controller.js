2126ca1049a436681a09c1d692f4d744181c6887
Ka Wai Yim <yimkawaii@gmail.com>
2023-10-06 05:30:59 -0400

    set up

--- Express_Assignment1/app/controllers/index.server.controller.js
+++ Express_Assignment1/app/controllers/index.server.controller.js
@@ -1,3 +1,5 @@
-exports.render = function(req, res) {
-    res.send('Hello World');
-    };
+exports.render = function (req, res) {
+    res.render('index', {
+        title: 'Ka Wai - Portolio'
+    })
+};
--- Express_Assignment1/app/routes/index.server.routes.js
+++ Express_Assignment1/app/routes/index.server.routes.js
@@ -1,4 +1,20 @@
-module.exports = function(app) {
-    var index = require('../controllers/index.server.controller');
-    app.get('/', index.render);
-    };
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Ka Wai - Portolio' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+router.get('/products', function(req, res, next) {
+  res.render('products', { title: 'Products' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+module.exports = router;
